31b0f6d2d00c18a647f1c5e73f8074096a2e6e45
Sudarsan <appsby12@gmail.com>
2022-11-18 23:05:55 +0100

    [WIP] Adding compute node functionalities

--- src/compute_node.py
+++ src/compute_node.py
@@ -2,10 +2,21 @@ class compute_node:
     def __init__(self):
         pass
         self.JobQ = []
-
+        self.cache = cache
     def enqueue_job(self, job):
         self.JobQ.append(job)
     
     def dequeue_job(self):
-        pass
-        
+        #FIXME: Should add queue time!
+        job = self.JobQ[0]
+        del self.JobQ[0]
+        return job
+    
+    def compute(self, job):
+        job.add_elapsed_time(job.compute_time)
+        storage_time = 0
+        #Check cache
+
+        #If miss
+        #Query storage
+        job.add_elapsed_time(storage_time)
