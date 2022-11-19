8861c5a9580dcb8d1d02833d4468e78927905d2d
Sudarsan <appsby12@gmail.com>
2022-11-19 13:53:16 +0100

    Changed queueing addition

--- src/compute_node.py
+++ src/compute_node.py
@@ -1,23 +1,36 @@
+import cache
+
 class compute_node:
-    def __init__(self):
-        pass
+    def __init__(self, start_time, time, storage):
         self.JobQ = []
-        self.cache = cache
+        self.cache = cache() 
+        self.time = time #Time obj
+        self.storage = storage #Storage obj
+
     def enqueue_job(self, job):
         self.JobQ.append(job)
     
     def dequeue_job(self):
-        #FIXME: Should add queue time!
         job = self.JobQ[0]
         del self.JobQ[0]
+        job.queue_time = self.time.get_curr_ts() - job.start_time
         return job
-    
+
     def compute(self, job):
-        job.add_elapsed_time(job.compute_time)
+        #Add compute time
+        job_elapsed_time = job.compute_time
+        
+        #Add storage time
         storage_time = 0
         #Check cache
         if self.cache.check_cache(job.storage_id):
             storage_time += self.cache_retrivel_time
         else:
             storage_time += self.storage.request_time(job.storage_id)
-        job.add_elapsed_time(storage_time)
+       
+        job_elapsed_time += storage_time
+        job.compute_time = job_elapsed_time
+        #Ack the job, which increments the time
+    
+    def start(self):
+        pass
