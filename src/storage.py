dda5f3afe1b97390c7fd4e801bfa92ef5028185c
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2022-12-11 23:22:39 +0100

    added a model for latency

--- src/storage.py
+++ src/storage.py
@@ -1,6 +1,29 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
 class StorageSystem:
     def __init__(self, retrieval_time):
         self.retrieval_time = retrieval_time
     def get_retrieval_time(self, size):
-        #Constant retrival time for now!
+        # Incoming Size in bytes
+        # We take our figures from http://www.paralleldatageneration.org/download/wbdb/WBDB2012_IN_15_Panda_HDFSBenchmark.pdf
+        # We generalize our throughput as 1400 MBps
+        size_in_bytes = 1400*1024*1024
+        # time in ms !!
+        tot_time = 0
+        tot_time += (size/size_in_bytes)*1400
+        # Modelling the Latency
+        # Max File Size = 10 GB
+        # x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        # y = [2, 5, 8, 10, 15, 18, 26, 30, 33, 39, 44]
+        # plt.plot(x, y)
+        # tot_time += np.interp(size/(1024*1024*1024), x, y) * 1000
+        # We get the equation as y = 4.34x - 0.86
+        latency = 4.34*(size/size_in_bytes) - 0.86
+        tot_latency = 0
+        if(latency < 0):
+            tot_latency = 4.34*(size/size_in_bytes)
+        else:
+            tot_latency = latency
+        tot_time += tot_latency*1000
         return self.retrieval_time
