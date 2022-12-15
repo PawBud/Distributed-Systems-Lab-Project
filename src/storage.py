5d3d3b2aed76837ae04a00de2b34ffe6b5d50b65
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2022-12-15 02:21:27 +0100

    .

--- src/storage.py
+++ src/storage.py
@@ -11,7 +11,9 @@ class StorageSystem:
         size_in_bytes = 1400*1024*1024
         # time in ms !!
         tot_time = 0
-        tot_time += (size/size_in_bytes)*1400
+        # The paper shows the results for a 10 GB file instead of a 1 GB file which we want
+        size_in_gigs = size/(size_in_bytes*10)
+        tot_time += size_in_gigs*1400
         # Modelling the Latency
         # Max File Size = 10 GB
         # x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -19,10 +21,10 @@ class StorageSystem:
         # plt.plot(x, y)
         # tot_time += np.interp(size/(1024*1024*1024), x, y) * 1000
         # We get the equation as y = 4.34x - 0.86
-        latency = 4.34*(size/size_in_bytes) - 0.86
+        latency = 4.34*size_in_gigs - 0.86
         tot_latency = 0
         if(latency < 0):
-            tot_latency = 4.34*(size/size_in_bytes)
+            tot_latency = 4.34*size_in_gigs
         else:
             tot_latency = latency*1000
         tot_time += tot_latency*1000
