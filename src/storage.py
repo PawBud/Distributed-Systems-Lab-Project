c8e7c7902d62380a7dbf878243294d83d72f9997
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2022-12-15 02:04:32 +0100

    added a comment regarding the error

--- src/storage.py
+++ src/storage.py
@@ -27,4 +27,5 @@ class StorageSystem:
             tot_latency = latency*1000
         tot_time += tot_latency*1000
         print("tot_time: ", tot_time)
+        # Counting time which is less than 1 ms as 0
         return int(tot_time)
